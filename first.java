04701d6b7d1bee9eba7d1c49bb2f98d4689c1bd0
sayaanfrah <sayaanfrah@gmail.com>
2023-10-08 03:29:28 +0530

    update

--- first.java
+++ first.java
@@ -1,18 +1,34 @@
+import java.util.*;
+
 public class first {
     public static void main(String[] args) {
-        int number = 4;
-        switch (number) {
+        Scanner sc = new Scanner(System.in);
+        int day = sc.nextInt();
+        switch (day) {
             case 1:
-                System.out.println("Samosa");
+                System.out.println("Monday");
                 break;
             case 2:
-                System.out.println("Idli");
+                System.out.println("Tuesday");
                 break;
             case 3:
-                System.out.println("Chowmin");
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Frdiay");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
                 break;
             default:
-                System.out.println("saniya babu got awake and started doing poti");
+                System.out.println("No  week listed ");
+
         }
     }
 }
--- index.js
+++ index.js
@@ -2,7 +2,32 @@
 // if (firstName=='Sayaan'){
 //     console.log(`Welcome ${firstName} to our website`)
 // }
-let color="red";
-if(color==="red"){
-    console.log(`the color is ${color} now you must wait`)
+// let color="red";
+// if(color==="red"){
+//     console.log(`the color is ${color} now you must wait`)
+// }
+
+// let size="S";
+// if(size==="XL"){
+//     console.log(`the price for the Popcorn of size ${size} is Rupees 250`)
+// }else if(size==="L"){
+//     console.log(`the price for the Popcorn of size ${size} is Rupees 150`)
+// }else if(size==="M"){
+//     console.log(`the price for the Popcorn of size ${size} is Rupees 100`)
+// }else if(size==="S"){
+//     console.log(`the price for the Popcorn of size ${size} is Rupees 50`)
+// }
+
+//nested if-else
+let days=3;
+switch(days){
+    case 1:console.log("Monday");
+    break;
+    case 2:console.log("Tuesday");
+    break;
+    case 3:console.log("wednesday");
+    break;
+    case 4:console.log("Thursday") ;
+    break;
+    default:console.log("no time left")
 }
