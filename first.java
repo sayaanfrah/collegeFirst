e4b08d12ee8683aae39ea799debd7027a346df72
sayaanfrah <sayaanfrah@gmail.com>
2023-10-08 00:29:23 +0530

    update

---
+++ first.java
@@ -0,0 +1,18 @@
+public class first {
+    public static void main(String[] args) {
+        int number = 4;
+        switch (number) {
+            case 1:
+                System.out.println("Samosa");
+                break;
+            case 2:
+                System.out.println("Idli");
+                break;
+            case 3:
+                System.out.println("Chowmin");
+                break;
+            default:
+                System.out.println("saniya babu got awake and started doing poti");
+        }
+    }
+}
