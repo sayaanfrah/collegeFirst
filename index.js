5a4046f6fa5bd8eeabac7b7fd2320863014ecaa4
sayaanfrah <sayaanfrah@gmail.com>
2023-10-07 20:05:00 +0530

    lastupdate

---
+++ index.js
@@ -0,0 +1,8 @@
+// let firstName='Sayaan';
+// if (firstName=='Sayaan'){
+//     console.log(`Welcome ${firstName} to our website`)
+// }
+let color="red";
+if(color==="red"){
+    console.log(`the color is ${color} now you must wait`)
+}
